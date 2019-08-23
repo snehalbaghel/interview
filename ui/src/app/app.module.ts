c0edf0ffa3e8ca1fbfa4bf4276bd06a33e7043d7
krunal-lia <krunallia18@gmail.com>
2019-08-23 19:38:44 +0530

    Added login form

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { LoginComponent } from './auth/components/login/login.component';
+import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    LoginComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LoginComponent]
 })
 export class AppModule { }
